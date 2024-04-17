d6825a45abe172cf5f65522f1333775ded2575d9
Voloshkevych <voloshkevych.b@gmail.com>
2024-04-17 21:44:45 +0300

    It seems to be working

---
+++ myapp/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/segmentio/kafka-go"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+)
+
+// DomainURL represents a simple struct for JSON request parsing
+type DomainURL struct {
+	Domain string `json:"domain"`
+	URL    string `json:"url"`
+}
+
+var (
+	rdb     *redis.Client
+	kafkaW  *kafka.Writer
+	db      *sql.DB
+	mongoDB *mongo.Collection
+	ctx     = context.Background()
+)
+
+func main() {
+	setupServices()
+	http.HandleFunc("/post", postHandler)
+	http.HandleFunc("/get", getHandler)
+	http.ListenAndServe(":8080", nil) // Simplified without log.Fatal
+}
+
+func setupServices() {
+	// Setting up Redis
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST") + ":6379",
+	})
+
+	// Setting up Kafka writer
+	kafkaW = &kafka.Writer{
+		Addr:     kafka.TCP(os.Getenv("KAFKA_HOST") + ":9092"),
+		Topic:    "urls",
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	// Setting up MySQL
+	db, _ = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/%s",
+		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DB")))
+
+	// Setting up MongoDB
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI(
+		fmt.Sprintf("mongodb://%s:%s@%s:27017", os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_HOST"))))
+	mongoDB = client.Database("mydb").Collection("urls")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	var du DomainURL
+	json.NewDecoder(r.Body).Decode(&du)
+
+	// Write operations simplified without error checks
+	rdb.Set(ctx, du.Domain, du.URL, 0)
+	kafkaW.WriteMessages(ctx, kafka.Message{Key: []byte(du.Domain), Value: []byte(du.URL)})
+	db.Exec("INSERT INTO urls (domain, url) VALUES (?, ?) ON DUPLICATE KEY UPDATE url = VALUES(url)", du.Domain, du.URL)
+	mongoDB.ReplaceOne(ctx, bson.M{"domain": du.Domain}, bson.M{"domain": du.Domain, "url": du.URL}, options.Replace().SetUpsert(true))
+
+	w.WriteHeader(http.StatusOK) // Always return OK for simplicity
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	domain := r.URL.Query().Get("domain")
+	url, _ := rdb.Get(ctx, domain).Result()
+	w.Write([]byte(url)) // Directly return the URL or an empty string if not found
+}
